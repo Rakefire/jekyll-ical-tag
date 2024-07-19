fdec01efcdfb3127979cc0816427dc22d8b6a4f0
Jos O'shea <jos.oshea.ky@gmail.com>
2024-07-18 23:56:39 -0400

    fix duplicate on first occurence of recurring event

--- lib/jekyll-ical-tag/calendar_parser.rb
+++ lib/jekyll-ical-tag/calendar_parser.rb
@@ -17,8 +17,30 @@ module Jekyll
 
       private
 
+<<<<<<< Updated upstream
       def parsed_feed
         Icalendar::Event.parse(@raw_feed)
+=======
+      def parsed_events
+        events = Icalendar::Event.parse(@raw_feed)
+
+        recurring_events =
+          events.flat_map do |event|
+            next unless event.rrule.present?
+
+            event
+              .occurrences_between(@recurring_start_date, @recurring_end_date).drop(1)
+              .map do |occurrence|
+                event.dup.tap do |e| # return a new event with the same attributes, but different start and end times
+                  e.dtstart = occurrence.start_time
+                  e.dtend = occurrence.end_time
+                end
+              end
+          end
+          .compact
+
+        events.concat(recurring_events)
+>>>>>>> Stashed changes
       end
     end
   end
