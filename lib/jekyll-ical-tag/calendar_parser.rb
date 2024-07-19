f70ff962f55671aa8cc4b0aec26cfccb1b4c5920
Ricky Chilcott <ricky@missionmet.com>
2024-07-19 07:03:41 -0400

    Refactor for clarity

--- lib/jekyll-ical-tag/calendar_parser.rb
+++ lib/jekyll-ical-tag/calendar_parser.rb
@@ -24,21 +24,24 @@ module Jekyll
         events = Icalendar::Event.parse(@raw_feed)
 
         recurring_events =
-          events.flat_map do |event|
-            next unless event.rrule.present?
-
-            event
-              .occurrences_between(@recurring_start_date, @recurring_end_date).drop(1)
-              .map do |occurrence|
-                event.dup.tap do |e| # return a new event with the same attributes, but different start and end times
-                  e.dtstart = occurrence.start_time
-                  e.dtend = occurrence.end_time
-                end
-              end
-          end
-            .compact
+          events
+            .select(&:rrule)
+            .flat_map do |event|
+              event
+                .occurrences_between(@recurring_start_date, @recurring_end_date)
+                .drop(1) # drop the first occurrence, as it is already included in the events array
+                .map { |occurrence| build_occurance_event(event, occurrence) }
+            end
 
         events.concat(recurring_events)
+      end
+
+      # return a new event with the same attributes, but different start and end times
+      def build_occurance_event(event, occurrence)
+        event.dup.tap do |e|
+          e.dtstart = occurrence.start_time
+          e.dtend = occurrence.end_time
+        end
       end
     end
   end
